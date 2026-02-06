1a9a35d4a3ba8a00c6fb9c7220a997912399f033
Riana <riana.aldias@gmail.com>
2026-02-06 09:22:05 -0300

    arquivo informação

---
+++ Fundamentos/Exemplos/informacao.js
@@ -0,0 +1,13 @@
+const curso = "Técnico em Desenvolvimento de Sistemas"
+const ano = 2025
+const NotaMinimaAprovacao = 50
+const NotaAprovacao = 49.9
+const cursoTecnico = true
+
+console.log('Minhas informações acadêmicas: ')
+console.log('Curso: ' + curso)
+console.log(`Ano: ${ano}`) /*Template String*/
+console.log(`Nota mímina para aprovação: ${NotaMinimaAprovacao}`)
+console.log('Nota para ser reprovado: ' + NotaAprovacao)
+console.log(`É um curso técnico? 
+    ${cursoTecnico}`)
